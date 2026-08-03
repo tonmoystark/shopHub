56450b1a96068063953141ea7efcbd69a80222f5
tonmoystark <tonmoy.a009@gmail.com>
2026-08-03 18:35:30 +0600

    Product module goin on

---
+++ database/migrations/2026_08_03_122852_create_products_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('category_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+
+            $table->string('name');
+
+            $table->string('slug')->unique();
+
+            $table->string('sku')->unique();
+
+            $table->text('description')->nullable();
+
+            $table->decimal('price', 10, 2);
+
+            $table->decimal('sale_price', 10, 2)->nullable();
+
+            $table->unsignedInteger('stock')->default(0);
+
+            $table->boolean('status')->default(true);
+
+            $table->boolean('is_featured')->default(false);
+
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
